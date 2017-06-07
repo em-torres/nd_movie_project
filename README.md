b690168ab0e301f337fe67cfced15a2f95266c84
Edwin Marte Torres <kdgpro15@gmail.com>
2017-06-07 19:23:51 -0400

    Refactoring Project

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # FSND Movie Project
 
 Udacity's Full Stack Nano Degree Movie Project made with Flask and Bulma. 
+
+
+## Installation:
+
+* Download the repo and create a `.env` file for the Decouple Configurations in configs. Take the `.env.example` file as
+template to create your own `.env` file
+
+* After that, in the command shell input `python run.py` and the app will be up and running.
--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+from decouple import config as Decouple
+from dj_database_url import parse as db_url
 import os
 
 # Define the application directory
@@ -6,13 +8,17 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     # Statement for enabling the development environment
-    DEBUG = False
+    DEBUG = Decouple('DEBUG')
     # Statement for enabling the testing mode
-    TESTING = False
+    TESTING = Decouple('TESTING')
 
     # Define the database - we are working with
     # SQLite for this example
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'movie_center.db')
+    SQLALCHEMY_DATABASE_URI = Decouple(
+        'DATABASE_URL',
+        default='sqlite:///' + os.path.join(BASE_DIR, 'movie_center.db'),
+        cast=db_url
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DATABASE_CONNECT_OPTIONS = {}
 
@@ -26,21 +32,9 @@ class Config:
     CSRF_ENABLED = True
     # Use a secure, unique and absolutely secret key for
     # signing the data.
-    CSRF_SESSION_KEY = "secret"
+    CSRF_SESSION_KEY = Decouple('CSRF_SECRET_KEY')
     # Secret key for signing cookies
-    SECRET_KEY = "secret"
+    SECRET_KEY = Decouple('SECRET_KEY')
 
     # Configuration for the Flask-Bcrypt Extension
-    BCRYPT_LEVEL = 12
-
-
-class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'movie_center_prod.db')
-
-
-class DevelopmentConfig(Config):
-    DEBUG = True
-
-
-class TestingConfig(DevelopmentConfig):
-    TESTING = True
+    BCRYPT_LEVEL = Decouple('BCRYPT_LEVEL')
